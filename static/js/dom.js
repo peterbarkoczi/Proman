7f1a25084136de2b2120ffcc7c5e27f62710a172
Peter Barkoczi <barkopetya@gmail.com>
2019-06-03 11:58:48 +0200

    create repository

---
+++ static/js/dom.js
@@ -0,0 +1,61 @@
+// It uses data_handler.js to visualize elements
+import {dataHandler} from "./data_handler.js";
+
+export let dom = {
+    _appendToElement: function (elementToExtend, textToAppend, prepend = false) {
+        // function to append new DOM elements (represented by a string) to an existing DOM element
+        let fakeDiv = document.createElement('div');
+        fakeDiv.innerHTML = textToAppend.trim();
+
+        for (let childNode of fakeDiv.childNodes) {
+            if (prepend) {
+                elementToExtend.prependChild(childNode);
+            } else {
+                elementToExtend.appendChild(childNode);
+            }
+        }
+
+        return elementToExtend.lastChild;
+    },
+    init: function () {
+        // This function should run once, when the page is loaded.
+    },
+    hideLoading: function () {
+        document.querySelector('#loading').style.display = 'none';
+    },
+    loadBoards: function () {
+        // retrieves boards and makes showBoards called
+        dataHandler.getBoards((boards) => {
+            this.showBoards(boards);
+            this.hideLoading();
+        });
+    },
+    showBoards: function (boards) {
+        // shows boards appending them to #boards div
+        // it adds necessary event listeners also
+
+        let boardList = '';
+
+        for (let board of boards) {
+            boardList += `
+                <li>${board.title}</li>
+            `;
+        }
+
+        const outerHtml = `
+            <ul class="board-container">
+                ${boardList}
+            </ul>
+        `;
+
+        this._appendToElement(document.querySelector('#boards'), outerHtml);
+    },
+    loadCards: function (boardId) {
+        // retrieves cards and makes showCards called
+    },
+    showCards: function (cards) {
+        // shows the cards of a board
+        // it adds necessary event listeners also
+    },
+    // here comes more features
+};
