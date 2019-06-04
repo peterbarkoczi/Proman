b2eda5847d439e2943d751947588d26ac96c51af
adamzopcsak <adamzopcsak@gmail.com>
2019-06-04 15:21:46 +0200

    create board button added

--- static/js/main.js
+++ static/js/main.js
@@ -6,7 +6,28 @@ function init() {
     dom.init();
     // loads the boards to the screen
     dom.loadBoards();
+    initEventListeners();
+}
 
+
+function initEventListeners() {
+    newBoardListener();
+}
+
+function newBoardListener() {
+    const createBoardButton = document.querySelector("#create-board");
+    const boards = document.querySelector("#boards");
+
+    createBoardButton.addEventListener('click', function () {
+
+        const boardCreator = document.createElement("div");
+        boardCreator.setAttribute('id', 'board-creator');
+        boardCreator.innerHTML = `<input type="text" id="new-board-name">
+                                  <button id="save-button">Save</button>`;
+
+        createBoardButton.appendChild(boardCreator);
+
+    });
 }
 
 init();
